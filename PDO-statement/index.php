ab2592fd5df8ac9e2006f5ef981b86d54119fa7b
Renata Kilian <renatakiliandev@gmail.com>
2017-05-02 21:44:46 -0300

    Update index.php

--- PDO-statement/index.php
+++ PDO-statement/index.php
@@ -4,10 +4,10 @@ $u = new Usuarios();
 
 $u->excluir(12); //teste excluir
 
-//$u->atualizar("Felipe2", "felipe@b7web.com.br", "123", 12);//teste para alterar
+//$u->atualizar("Felipe2", "felipe@b7web.com.br", "123", 12); //teste para alterar
 
-//$u->inserir("Felipe", "felipe@b7web.com.br", "123"); //teste inserir
+//$u->inserir("Felipe", "felipe@b7web.com.br", "123"); //teste para inserir
 
 //$res = $u->selecionar(2); //peguei id 1 e vai guardar os results na var $res
 
-//print_r($res); //p/ ver o result na tela
+//print_r($res); //para ver o resultado na tela
