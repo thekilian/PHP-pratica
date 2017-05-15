1b608a1b36c2f392e535a7029d7be2f2d798091b
Renata Kilian <renatakiliandev@gmail.com>
2017-05-15 18:01:00 -0300

    update calculo.php calc-imc

--- programmr-exercises/calc-imc/calculo.php
+++ programmr-exercises/calc-imc/calculo.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:programmr-exercises/calc-imc/calculo.php
 <?php
 /* BMI Calculator
 http://www.programmr.com/practice/phpcourse_sandbox_1971/node/2053
@@ -76,32 +75,3 @@ echo "BMI Category: ".$category;
 echo "<br/><br/>";
 echo "<a href='index.php'>Voltar</a>";
 exit;
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Calculadora IMC</title>
-	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-</head>
-<body>
-	<div class="container">
-		<h2>Calculadora de IMC</h2>
-		<hr/>
-		
-		<form method="POST" action="calculo.php">
-			<div class="form-group">
-				<input class="form-control" type="number" name="height" placeholder="Please type your height">
-			</div>
-			<div class="form-group">
-				<input class="form-control" type="number" name="weight" placeholder="Please type your weight">
-			</div>
-				<button class = "btn btn-success btn-block">Calculate</button>
-		</form>
-	</div>
-
-<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
-</body>
-</html>
->>>>>>> origin/master:Calc-IMC/index.php
