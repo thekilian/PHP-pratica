8f11ba58ecb70c3842edb30e27a8e9e66f878b04
Renata Kilian <renatakiliandev@gmail.com>
2017-05-16 18:24:29 -0300

    Delete teste

--- TAKISGEN/teste-update-controle/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['id']) && empty($_SESSION['id']) == false) {
-	echo "Área restrita";
-	header("Location: controle.php");
-
-} else {
-	header("Location: login.php");
-}
