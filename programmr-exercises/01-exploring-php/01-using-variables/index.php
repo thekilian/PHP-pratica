70bcec76db6362ff88400445df0ce0ea222aa30d
Renata Kilian <renatakiliandev@gmail.com>
2017-06-16 13:40:31 -0300

    Resolução exercício using-variables (programmr-exercises)

--- programmr-exercises/01-exploring-php/01-using-variables/index.php
+++ programmr-exercises/01-exploring-php/01-using-variables/index.php
@@ -4,22 +4,28 @@ http://www.programmr.com/practice/phpcourse_sandbox_1971/node/2031
 
 Three variables have been declared and values have been assigned to each of them. Display the values of these three variables on the screen, as shown below.
 
- 
 This is room #113 
 e is close to 2.71828
 I am learning a bit about Computer Science
 
 #PHP
-<?php
-
 $a="113";
 $b="2.71828";
 $c="Computer Science";
 //{ write ur logic here
 
-
-
-
 //}
-?>
 -->
+
+<?php
+$a = "113";
+$b = "2.71828";
+$c = "Computer Science";
+
+echo "This is room #".$a;
+echo "<br/>";
+echo "and is close to ".$b;
+echo "<br/>";
+echo "I am learning a bit about ".$c;
+
+?>
