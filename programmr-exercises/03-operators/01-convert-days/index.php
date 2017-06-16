a6e647850153b958e3ba7f29070d5898d6bca3cb
Renata Kilian <renatakiliandev@gmail.com>
2017-06-16 13:47:59 -0300

    Resolução exercício convert-days (programmr-exercises)

--- programmr-exercises/03-operators/01-convert-days/index.php
+++ programmr-exercises/03-operators/01-convert-days/index.php
@@ -22,3 +22,38 @@ Years-1 Weeks-80
   echo "Years-".intval($years)." Weeks-".intval($weeks);
   exit;
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Convert days to years and weeks</title>
+</head>
+<body>
+	<form method="POST">
+		<label>Enter days:</label><br/>
+		<input type="number" name="days" placeholder="Enter days here" required/><br/><br/>
+		<input type="submit" value="Check"/><br/>
+	</form>
+	<hr/>
+</body>
+</html>
+
+<?php
+$years = 0;
+$weeks = 0;
+
+if(isset($_POST['days']) && !empty($_POST['days'])) {
+	$days = $_POST['days'];
+
+	$years = $days / 365;
+	$weeks = $days / 7;
+
+}
+
+echo "Years: ".intval($years);
+echo "<br/>";
+echo "Weeks: ".intval($weeks);
+exit;
+
+?>
