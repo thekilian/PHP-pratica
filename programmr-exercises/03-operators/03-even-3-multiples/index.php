3cf1a507dcba98267267e77ec10defcac9f864e1
Renata Kilian <renatakiliandev@gmail.com>
2017-06-16 19:50:06 -0300

    Resolução exercício even 3 multiple (programmr-exercises)

--- programmr-exercises/03-operators/03-even-3-multiples/index.php
+++ programmr-exercises/03-operators/03-even-3-multiples/index.php
@@ -15,11 +15,39 @@ echo "Enter the Number:";
     //{
     //write down your logic here
   
-  
-  
-  
-  
-  
     //}
   exit;
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Even 3 multiples</title>
+</head>
+<body>
+	<form method="POST">
+		<label>Enter the number:</label><br/>
+		<input type="number" name="number" placeholder="Enter the number here" required/><br/><br/>
+		<input type="submit" value="Is it an even multiple of 3?"/>
+	</form>
+	<hr/>
+</body>
+</html>
+
+<?php
+if(isset($_POST) && !empty($_POST)) {
+	$answer = 0;
+	$number = $_POST['number'];
+
+	if(($number % 3) == 0) {//se n° for multiplo de 3
+		if($number % 2 == 0) { //even
+			echo "yes";
+		} else {
+			echo "no"; //odd
+		}
+	} else { // se não for múltiplo de 3
+		echo "Please, enter a multiple of 3.";
+	}
+}
+exit;
