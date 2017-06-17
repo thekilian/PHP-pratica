d4b1319a9971d8c338a5d88cc3ee0ebb33b19730
Renata Kilian <renatakiliandev@gmail.com>
2017-06-16 21:05:38 -0300

    Resolução exercício temperature-converter (programmr-exercises)

--- programmr-exercises/03-operators/04-temperature-converter/index.php
+++ programmr-exercises/03-operators/04-temperature-converter/index.php
@@ -26,13 +26,52 @@ echo "Enter your choice :<br/>";
     $optn=trim(fgets(STDIN));
     //{Write your code here 
     
-    
-    
-    
-    
-    
-    
-    
     //}
   exit;
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Temperature Converter</title>
+</head>
+<body>
+	<form method="POST">
+		<label>Enter your choice:<br/>
+			<strong>1. </strong>Fahrenheit to Celsius:<br/>
+			<strong>2. </strong>Celsius to Fahrenheit:
+		</label><br/>
+		<input type="number" name="choice" placeholder="Enter your choice here" required/><br/><br/>
+		
+		<label>Enter the temperature:</label><br/>
+		<input type="number" name="conv" placeholder="Enter temperature here" required/><br/>
+		<input type="submit" value="Temperature Converter"/><br/><br/>
+	</form>
+	<hr/>
+</body>
+</html>
+
+<?php
+if(isset($_POST) && !empty($_POST)) {
+	$conv = 0;
+	$choice = $_POST['choice'];
+
+	if($choice == 1) {
+		$conv = ($_POST['conv'] - 32) / 1.8;
+		echo "Celsius: ".$conv;
+	} else {
+		$conv = ($_POST['conv'] * 1.8) + 32;
+		echo "Fahrenheit: ".$conv;
+	}
+	exit;
+}
+
+/*
+Para converter graus Fahrenheit em graus Celsius, subtraia 32 e divida por 1,8.
+°C = (°F − 32) ÷ 1, 8
+
+Para converter graus Celsius em graus Fahrenheit, multiplique por 1,8 e adicione 32.
+°F = °C × 1, 8 + 32
+*/
+?>
