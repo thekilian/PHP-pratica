4da8d66cd3f97754ba6b0f39aca297662a26bcb8
Renata Kilian <renatakiliandev@gmail.com>
2017-06-19 14:04:21 -0300

    Resolução digit-100-place (programmr-exercises)

--- programmr-exercises/03-operators/07-digit-100-place/index.php
+++ programmr-exercises/03-operators/07-digit-100-place/index.php
@@ -23,3 +23,34 @@ Enter Number:346
   echo $ans;
   exit;
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Digit at 100's place</title>
+</head>
+<body>
+	<form method="POST">
+		<label>Enter number:</label><br/>
+		<input type="number" step="any" name="number" placeholder="Enter number" required/><br/><br/>
+
+		<input type="submit" value="Check"/>
+	</form>
+	<hr/>
+</body>
+</html>
+
+<?php
+
+$ans = 0;
+
+if(isset($_POST['number']) && !empty($_POST['number'])) {
+	$number = $_POST['number'];
+
+	$ans = substr($number, 0, 1);
+
+	echo $ans;
+}
+exit;
+?>
