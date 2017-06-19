9586a6bd22565342e15978f2b7169d65e4df98f4
Renata Kilian <renatakiliandev@gmail.com>
2017-06-19 14:03:48 -0300

    Resolução dumb-calculator (programmar-exercises)

--- programmr-exercises/03-operators/06-dumb-calculator/index.php
+++ programmr-exercises/03-operators/06-dumb-calculator/index.php
@@ -33,11 +33,45 @@ $result=0;
    
      ///{ WRITE YOUR CODE HERE
 
-    
-    
-    
-      
      ///} 
  
   exit;
 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Dumb Calculator</title>
+</head>
+<body>
+	<form method="POST">
+		<label>Enter the 1st number:</label><br/>
+		<input type="number" step="any" name="number1" placeholder="Enter the 1st number" required/><br/><br/>
+
+		<label>Enter the 2nd number:</label><br/>
+		<input type="number" step="any" name="number2" placeholder="Enter the 2nd number" required/><br/><br/>
+
+		<label>Enter the 3rd number:</label><br/>
+		<input type="number" step="any" name="number3" placeholder="Enter the 3rd number" required/><br/><br/>
+
+		<input type="submit" value="Calculate"/>
+	</form>
+	<hr/>
+</body>
+</html>
+
+<?php
+
+$result = 0;
+
+if(isset($_POST) && !empty($_POST)) {
+	$number1 = $_POST['number1'];
+	$number2 = $_POST['number2'];
+	$number3 = $_POST['number3'];
+
+	$result = ($number1 + $number2 + $number3) / 2;
+
+	echo $result;
+}
+exit;
+?>
