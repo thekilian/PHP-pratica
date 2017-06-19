790c64cc95d9cd1d55500fb1bf814a7c655832a2
Renata Kilian <renatakiliandev@gmail.com>
2017-06-19 19:21:59 -0300

    Resolução find-modulus (programmr-exercises)

--- programmr-exercises/03-operators/12-find-modulus/index.php
+++ programmr-exercises/03-operators/12-find-modulus/index.php
@@ -26,12 +26,44 @@ Output:
    $number2=trim(fgets(STDIN));  
      //{  
      //write down your logic here  
-  
-  
-  
-	    
+
      //}   
   echo "modulus is:";  
   echo $mod;  
   exit;  
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Find the modulus of 2 numbers</title>
+</head>
+<body>
+  <form method="POST">
+    <label>Enter the 1st number:</label><br/>
+    <input type="number" step="any" name="number1" placeholder="Enter the 1st number" required/><br/><br/>
+
+    <label>Enter the 2nd number:</label><br/>
+    <input type="number" step="any" name="number2" placeholder="Enter the 2nd number" required/><br/><br/>
+
+    <input type="submit" value="Find the modulus!"/>
+  </form>
+  <hr/>
+</body>
+</html>
+
+<?php
+
+$mod = 0;
+
+if(isset($_POST) && !empty($_POST)) {
+  $number1 = $_POST['number1'];
+  $number2 = $_POST['number2'];
+
+  $mod = ($number1 % $number2);
+
+  echo "modulus is:".$mod;
+}
+exit;
+?>
