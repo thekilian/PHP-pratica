b4f3f87282c1d34557a57d0923b1fc70b4a8d99a
Renata Kilian <renatakiliandev@gmail.com>
2017-06-19 19:22:22 -0300

    Resolução body-mass-index (programmr-exercises)

--- programmr-exercises/03-operators/13-body-mass-index/index.php
+++ programmr-exercises/03-operators/13-body-mass-index/index.php
@@ -26,13 +26,45 @@ $bmi=0;
      //{
      //write down your logic here
     
-    
-    
-    
-    
      //} 
      $bmi = number_format($bmi, 5, '.', '');
   echo "BMI is:";
   echo $bmi;
   exit;
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Body Mass Index</title>
+</head>
+<body>
+  <form method="POST">
+    <label>Enter your height in m:</label><br/>
+    <input type="number" step="any" name="height" placeholder="Your height in m" required/><br/><br/>
+
+    <label>Enter your weight in kg:</label><br/>
+    <input type="number" step="any" name="weight" placeholder="Your weight in kg" required/><br/><br/>
+
+    <input type="submit" value="Body Mass Index"/>
+  </form>
+  <hr/>
+</body>
+</html>
+
+<?php
+
+$bmi = 0;
+
+if(isset($_POST) && !empty($_POST)) {
+  $height = $_POST['height'];
+  $weight = $_POST['weight'];
+
+  $bmi = $weight / ($height * $height);
+  $bmi = number_format($bmi, 5, '.', '');
+}
+  echo "BMI is:";
+  echo $bmi;
+  exit;
+?>
