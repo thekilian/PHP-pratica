b9085ca1d14029759334754f73af293a18b46cae
André Luiz Girol <andregirol@gmail.com>
2017-09-10 17:05:36 -0300

    Pindura sobre Escopo

---
+++ Escopo/functions.php
@@ -0,0 +1,15 @@
+<?php
+####### LEXICAL ENVIRONMENT #######
+function mostra_constante(){
+    echo "vai funcionar?";
+    echo CHUCK;
+}
+
+function mostra_url($sait){
+    echo $sait;
+}
+
+function soma($x,$y){
+    return $x + $y;
+}
+####### FIM DE L.E. ########
