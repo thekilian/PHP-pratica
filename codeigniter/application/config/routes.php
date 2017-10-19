971d580242e76c78cc3d93e10c0101074153af6a
Renata Kilian <renatakiliandev@gmail.com>
2017-10-19 17:58:16 -0200

    CodeIgniter Tutorial - News section

--- codeigniter/application/config/routes.php
+++ codeigniter/application/config/routes.php
@@ -49,5 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'pages/view';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
